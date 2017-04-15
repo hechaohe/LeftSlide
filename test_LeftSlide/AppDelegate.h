97734e2aef8be0dd998053bf9bd61c6c74d25f2a
hechaohe <550121308@qq.com>
2017-04-15 11:06:42 +0800

    Initial Commit

---
+++ test_LeftSlide/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test_LeftSlide
+//
+//  Created by hc on 2017/4/15.
+//  Copyright © 2017年 hc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ test_LeftSlide/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  test_LeftSlide
+//
+//  Created by hc on 2017/4/15.
+//  Copyright © 2017年 hc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
